a9f32afa6cd80f4beacef813c87132235133ea0a
Abiodun Adesina <101020483+Adesina-Abiodun@users.noreply.github.com>
2023-05-17 09:10:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict'
+// VARIABLES
+let bodyEl = document.querySelector("body");
+let closeBtn = document.querySelector(".close-icon");
+let navBtn = document.querySelector(".nav-btn");
+let navEl = document.querySelector(".navbar");
+let navItems = document.querySelector(".nav-list");
+let navLinks = document.querySelectorAll(".nav-link");
+let projectLogo = document.querySelector(".nav-logo");
