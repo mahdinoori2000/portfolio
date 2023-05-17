0e51dd85f062a63a40687ee50ad0612bd808c6b5
Mahdi Noori <mahdinoori2389@gmail.com>
2023-05-17 12:42:07 +0430

    Add functinality to the navbar button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 // VARIABLES
 let bodyEl = document.querySelector("body");
 let closeBtn = document.querySelector(".close-icon");
@@ -7,3 +7,26 @@ let navEl = document.querySelector(".navbar");
 let navItems = document.querySelector(".nav-list");
 let navLinks = document.querySelectorAll(".nav-link");
 let projectLogo = document.querySelector(".nav-logo");
+//FUNCTIONS
+function operations() {
+  closeBtn.classList.toggle("hidden");
+  navBtn.classList.toggle("hidden");
+  navEl.classList.toggle("nav-mobile");
+  bodyEl.classList.toggle("overflow");
+  projectLogo.classList.toggle("opacity");
+  navItems.classList.toggle("mobile-nav-list");
+}
+
+// JENERAL OPERATION
+navBtn.addEventListener("click", operations);
+closeBtn.addEventListener("click", operations);
+navLinks.forEach((element) => {
+  element.addEventListener("click", function () {
+    bodyEl.classList.remove("overflow");
+    navEl.classList.remove("nav-mobile");
+    navItems.classList.remove("mobile-nav-list");
+    closeBtn.classList.add("hidden");
+    navBtn.classList.remove("hidden");
+    projectLogo.classList.remove("opacity");
+  });
+});
