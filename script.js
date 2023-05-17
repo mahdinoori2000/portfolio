083776d5c5960c47f4534b66eb372004daf47f84
Mahdi Noori <mahdinoori2389@gmail.com>
2023-05-17 13:05:30 +0430

    Fix linter errors

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
-"use strict";
 // VARIABLES
-let bodyEl = document.querySelector("body");
-let closeBtn = document.querySelector(".close-icon");
-let navBtn = document.querySelector(".nav-btn");
-let navEl = document.querySelector(".navbar");
-let navItems = document.querySelector(".nav-list");
-let navLinks = document.querySelectorAll(".nav-link");
-let projectLogo = document.querySelector(".nav-logo");
-//FUNCTIONS
+const bodyEl = document.querySelector('body');
+const closeBtn = document.querySelector('.close-icon');
+const navBtn = document.querySelector('.nav-btn');
+const navEl = document.querySelector('.navbar');
+const navItems = document.querySelector('.nav-list');
+const navLinks = document.querySelectorAll('.nav-link');
+const projectLogo = document.querySelector('.nav-logo');
+// FUNCTIONS
 function operations() {
-  closeBtn.classList.toggle("hidden");
-  navBtn.classList.toggle("hidden");
-  navEl.classList.toggle("nav-mobile");
-  bodyEl.classList.toggle("overflow");
-  projectLogo.classList.toggle("opacity");
-  navItems.classList.toggle("mobile-nav-list");
+  closeBtn.classList.toggle('hidden');
+  navBtn.classList.toggle('hidden');
+  navEl.classList.toggle('nav-mobile');
+  bodyEl.classList.toggle('overflow');
+  projectLogo.classList.toggle('opacity');
+  navItems.classList.toggle('mobile-nav-list');
 }
 
 // JENERAL OPERATION
-navBtn.addEventListener("click", operations);
-closeBtn.addEventListener("click", operations);
+navBtn.addEventListener('click', operations);
+closeBtn.addEventListener('click', operations);
 navLinks.forEach((element) => {
-  element.addEventListener("click", function () {
-    bodyEl.classList.remove("overflow");
-    navEl.classList.remove("nav-mobile");
-    navItems.classList.remove("mobile-nav-list");
-    closeBtn.classList.add("hidden");
-    navBtn.classList.remove("hidden");
-    projectLogo.classList.remove("opacity");
+  element.addEventListener('click', () => {
+    bodyEl.classList.remove('overflow');
+    navEl.classList.remove('nav-mobile');
+    navItems.classList.remove('mobile-nav-list');
+    closeBtn.classList.add('hidden');
+    navBtn.classList.remove('hidden');
+    projectLogo.classList.remove('opacity');
   });
 });
