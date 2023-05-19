39436ae7b95ecd774759e9d46c7253bb85713d5d
Mahdi Noori <mahdinoori2389@gmail.com>
2023-05-19 14:54:05 +0430

    Add functionality to the contact form

--- script.js
+++ script.js
@@ -29,3 +29,22 @@ navLinks.forEach((element) => {
     projectLogo.classList.remove('opacity');
   });
 });
+
+// VALIDATIONA FORM
+
+const error = document.getElementById('error-message');
+const form = document.querySelector('.contact-form');
+const email = document.getElementById('user-email');
+const errorMessage = 'Please write your email address in lower case';
+// CODE
+form.addEventListener('submit', (event) => {
+  // prevent from submittion
+  event.preventDefault();
+  const userInput = email.value;
+  if (userInput !== userInput.toLowerCase()) {
+    error.innerText = errorMessage;
+  } else {
+    error.innerText = '';
+    form.submit();
+  }
+});
